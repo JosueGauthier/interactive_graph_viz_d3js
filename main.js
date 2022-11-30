dfb0647fde564dc7c947f54f7b7ce1b70114a7d1
JosueGauthier <josue.gauthier@gmail.com>
2022-11-30 19:25:08 +0100

    working

--- main.js
+++ main.js
@@ -15,24 +15,30 @@ window.onload = () => {
     width = screenWidth,
     height = screenHeight; */
 
-  var svg = d3.select(".graph").append("svg")
+  /*   var svg = d3.select(".graph").append("svg")
+      .attr("width", screenWidth)
+      .attr("height", screenHeight)
+      .append("g");
+  
+  
+  
+    var svg = d3.select("svg"),
+      width = +svg.attr("width"),
+      height = +svg.attr("height"); */
+
+
+  const svg = d3.select("svg")
     .attr("width", screenWidth)
     .attr("height", screenHeight)
-    .append("g");
 
-
-
-  var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+  var width = +svg.attr("width");
+  var height = +svg.attr("height");
 
   var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-  var linkWidth = d3.scaleLinear().range([0.2, 4]);
-  linkWidth.domain([0, d3.max(data.links, function (d) { return d.totalFlightBeetween; })]);
 
-  var r = d3.scaleLinear().range([3, 15]);
-  r.domain([0, d3.max(data.nodes, function (d) { return d.totalFlight; })]);
+
+
 
 
   var simulation = d3.forceSimulation()
@@ -42,6 +48,13 @@ window.onload = () => {
 
 
   d3.json("input.json").then(function (data) {
+
+
+    var linkWidth = d3.scaleLinear().range([0.2, 6]);
+    linkWidth.domain([0, d3.max(data.links, function (d) { return d.totalFlightBeetween; })]);
+
+    var r = d3.scaleLinear().range([3, 15]);
+    r.domain([0, d3.max(data.nodes, function (d) { return d.totalFlight; })]);
 
     var div = d3.select(".graph").append("div")
       .attr("class", "tooltip")
@@ -53,21 +66,35 @@ window.onload = () => {
       .selectAll("line")
       .data(data.links)
       .enter().append("line")
+      .attr("id", function(d) { return d.source+d.target})
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.8)
+      .attr("stroke-width", function (d) { return linkWidth(d.totalFlightBeetween) })
       .on("mouseover", function (d) {
         console.log(d.totalFlightBeetween)
+        console.log(d3.select(this).attr("class"))
+        console.log(d3.select(this).attr("id"))
         div.transition()
           .duration(200)
           .style("opacity", .9);
-        div.html(d.totalFlightBeetween + "<br>")
+        div.html("Total flights: "+d.totalFlightBeetween + "<br>"+"Arrivals: "+d.arrival+ "<br>"+"Departures: "+d.departure)
           .style("left", (d3.event.pageX) + "px")
           .style("top", (d3.event.pageY) + "px");
+        d3.select(this).attr("stroke", "red")
+        //d3.select(this).attr("stroke-opacity", 1)
       })
       .on("mouseout", function (d) {
+        d3.select(this).attr("stroke", "#999")
+        //d3.select(this).attr("stroke-opacity", 0.5)
         div.transition()
           .duration(500)
           .style("opacity", 0);
-      })
-      .attr("stroke-width", function (d) { return linkWidth(d.totalFlightBeetween) });
+      });
+
+      console.log("aaa")
+
+    
+    var isclicked = false;
 
     var node = svg.append("g")
       .attr("class", "nodes")
@@ -76,6 +103,7 @@ window.onload = () => {
       .enter().append("circle")
       .attr("r", function (d) { return r(d.totalFlight); })
       .attr("fill", function (d) { return color(d.group); })
+      
       .on("mouseover", function (d) {
         console.log(d.id)
         div.transition()
@@ -84,31 +112,39 @@ window.onload = () => {
         div.html(d.id + "<br>" + d.totalFlight)
           .style("left", (d3.event.pageX) + "px")
           .style("top", (d3.event.pageY) + "px");
+        d3.selectAll("[id*="+d.id+"]").attr("stroke", "red")
+
+        //d3.selectAll("line").attr("stroke-opacity", 1)
       })
       .on("mouseout", function (d) {
         div.transition()
           .duration(500)
           .style("opacity", 0);
+
+          d3.selectAll("line").attr("stroke", "#999")
       })
+
+      .on("click", function(d) { 
+
+        if (isclicked === false) {
+          d3.selectAll(":not([id*="+d.id+"])").attr("stroke-opacity", "0");
+          isclicked = !isclicked;
+          
+        }else{
+
+          d3.selectAll(":not([id*="+d.id+"])").attr("stroke-opacity", "1")
+          isclicked = !isclicked;
+
+        }
+
+        
+       })
       .call(d3.drag()
         .on("start", dragstarted)
         .on("drag", dragged)
         .on("end", dragended));
 
-    /*     node.append("text")
-          .text(function (d) {
-            return d.id;
-    
-          })
-          .style("font-size", 15)
-          .style("fill", "red")
-          .attr('alignment-baseline', 'middle')
-          .attr('x', 6)
-          .attr('y', 3);
-    
-        node.append("title")
-          .text(function (d) { return d.id; });
-     */
+
     simulation
       .nodes(data.nodes)
       .on("tick", ticked);
